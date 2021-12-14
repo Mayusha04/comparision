44026e3c81520046805447590d253bc2e1bc54af
mayusha <mayushachowdary95@gmail.com>
2021-12-14 21:24:51 +0530

    comparision

---
+++ compar.js
@@ -0,0 +1,58 @@
+if(8 == "8"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(4=== "4"){            
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(4 != "4"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+if("6" !== "6"){                
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(6 < "3"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+if(4 <="4"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(5 > "3"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+
+if(5 >= "5"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+if(30>50){
+    console.log("wrong");
+} else if(10<20){
+   console.log("right");
+}else {
+    console.log("choose another");
+}
